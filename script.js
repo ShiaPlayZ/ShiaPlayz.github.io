94950483ac2425f1fa591ba299128ae8d4698c5d
Shia Dayaram <shiadayaram@gmail.com>
2026-09-08 19:33:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function revealImage() {
+  document.getElementById('myButton').style.display = 'none';
+  document.getElementById('myImage').style.display = 'block';
+  document.getElementById('myText').style.display = 'block'; 
+}
